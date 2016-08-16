71752963c70677d6e5c306f839ec8450bd3073ba
vincent <vincent.claes@porphyrio.com>
2016-08-16 22:57:18 +0200

    use te original full map code

--- potential001.py
+++ potential001.py
@@ -1,63 +1,44 @@
-from flask import Flask, render_template, request, redirect
+# coding: utf-8
+
+from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
 from iotpotential.location import (Location, LastSeenLocation, LocationHistory)
 from threading import Thread
-
-
 
 app = Flask(__name__, template_folder="templates")
 
 # you can set key as config
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyDigFY04sXaaKV59x_-qR4tq5oCxcZwaYs"
 
-# Initialize the extension
-GoogleMaps(app)
+# you can also pass key here
+GoogleMaps(app, key="AIzaSyDigFY04sXaaKV59x_-qR4tq5oCxcZwaYs")
 
 
-
-@app.route('/google8119fc9c07d90f07.html')
-def googleverification():
-    return render_template('google8119fc9c07d90f07.html')
-
-@app.route('/fullmap')
+@app.route('/')
 def fullmap():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        #lat=37.4419,
-        #lng=-122.1419,
-        lat=LastSeenLocation.latitude,
-        lng=LastSeenLocation.longitude,
-        #markers=[(37.4419, -122.1419)]
-        polylines=[LocationHistory.location_history]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-     #LocationHistory.write_history()
-    return render_template('example_fullmap.html', mymap=mymap, sndmap=sndmap)
-   
-  
+    try:
+        fullmap = Map(
+            identifier="fullmap",
+            varname="fullmap",
+            style=(
+                "height:100%;"
+                "width:100%;"
+                "top:0;"
+                "left:0;"
+                "position:absolute;"
+                "z-index:200;"
+            ),
+            lat=LastSeenLocation.latitude,
+            lng=LastSeenLocation.longitude,
+           polylines=[LocationHistory.location_history]
+        )
+        return render_template('example_fullmap.html', fullmap=fullmap)
+    finally:
+        LocationHistory.write_history()
 
-if __name__ == '__main__':
-		#l=Location()
-    	#t=Thread(target=l.continuously_get_current_location)
-    	#t.start()
-    	app.run(debug=True, use_reloader=True)
+if __name__ == "__main__":
+    l = Location()
+    t = Thread(target=l.continuously_get_current_location)
+    t.start()
+    app.run(debug=True, use_reloader=True)
